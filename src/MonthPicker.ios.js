3583a22cb14867a263de5d5e82e4f07ccae1f122
Gustavo Paris <gustavo.paris@devborn.com>
2020-05-01 01:41:33 -0300

    eslint config

--- src/MonthPicker.ios.js
+++ src/MonthPicker.ios.js
@@ -6,24 +6,28 @@ import RNMonthPickerView from './RNMonthPickerNativeComponent';
 
 const styles = StyleSheet.create({
   container: { height: 200, minWidth: 315 },
-  picker: {flex: 1}
+  picker: { flex: 1 },
 });
 
 const MonthPicker = ({ onValueChange, restProps }) => {
   const onChange = useCallback(
     (event) => {
-      console.log("newDate", event.nativeEvent.newDate);
+      console.log('newDate', event.nativeEvent.newDate);
       // const date = moment(event.newDate).format("MM-YYYY");
       // onValueChange(date)
-    }
-    , [onValueChange]
+    },
+    [onValueChange],
   );
 
   return (
     <View style={styles.container}>
-      <RNMonthPickerView style={styles.picker} onChange={onChange} {...restProps}/>
+      <RNMonthPickerView
+        style={styles.picker}
+        onChange={onChange}
+        {...restProps}
+      />
     </View>
   );
-}
+};
 
 export default MonthPicker;
